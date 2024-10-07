f8b7c70ffada0866d0d2593788a16e62dfe3fcfe
JohnCarloPaz <jpaz@spcf.edu.ph>
2024-10-07 17:01:53 +0800

    Updated the landing page

--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -1,15 +1,40 @@
-import { BackgroundImage , Grid, Image } from '@mantine/core';
+import { BackgroundImage , Grid, Image, Stack, Button } from '@mantine/core';
 import { Login } from "../components/Login";
+import NavBar from '../components/NavBar';
 
 // Images
 import bgImage from "../assets/images/BG.png";
 import monitor from "../assets/images/3dMonitor.png";
+import chess from "../assets/images/chess.png";
+import puzzle from "../assets/images/puzzle.png";
+import avatar from "../assets/images/avatar.png";
+import medal from "../assets/images/medal.png";
+
+const menu = [
+  {
+    name: 'play',
+    asset: <Image src={chess} w='auto' fit='contain'/>,
+  },
+  {
+    name: 'learn',
+    asset: <Image src={puzzle} w='auto' fit='contain'/>,
+  },
+  {
+    name: 'customize',
+    asset: <Image src={avatar} w='auto' fit='contain'/>,
+  },
+  {
+    name: 'compete',
+    asset: <Image src={medal} w='auto' fit='contain'/>,
+  }
+];
 
 const Landing = () => {
   return (
     <>
       <div className="relative isolate min-h-[100dvh]">
         <BackgroundImage src={bgImage} h={4000}>
+          <NavBar></NavBar>
           <main className="mx-auto max-w-screen-2xl py-10 px-10 md:px-24 tall:min-h-[calc(100dvh-128px)]">
             <Grid>
               <Grid.Col span={{base: 12, md: 4, lg: 4, xl: 4}}>
@@ -23,13 +48,26 @@ const Landing = () => {
               <Grid.Col span={{base: 12, md: 4, lg: 4, xl: 5}} className='flex justify-center items-center'>
                 <Image src={monitor} w='auto' fit='contain'/>
               </Grid.Col>
+              <Grid.Col span={{base: 12, md: 4, lg: 4, xl: 3}} className='mx-0 my-auto'>
+              <Stack gap='lg' h='auto'>
+                {
+                  menu.map((menu, index) => (
+                      <Button key={index} variant='filled' color='white' fullWidth h='auto' leftSection={menu.asset} justify='space-between'>
+                        <p className='capitalize text-black'>
+                          {menu.name}
+                        </p>
+                      </Button>
+                    
+                  ))
+                }
+                </Stack>
+              </Grid.Col>
             </Grid>
-
-            <Login />
           </main>
         </BackgroundImage>
-      </div>
-    </>);
+        </div>
+    </>
+    );
 };
 
-export default Landing;
+export default Landing; 

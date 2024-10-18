1d3666f7fe71bf8e18f603691bbdc871f7b680f2
melmatx <melpalana13@gmail.com>
2024-10-18 18:46:10 +0800

    Initial Juno deploy config

---
+++ juno.config.mjs
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'nh5bg-raaaa-aaaal-amqbq-cai',
+    source: 'dist'
+  }
+});
